5ae7c602f388665a85224ddcc73d3ac40a9e0431
Silambarasan <simbu2dev@gmail.com>
2025-05-13 15:23:23 +0530

    Added beds API service

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+const bedRouter = require("./routes/beds");
+
+app.use("/beds", bedRouter);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running at ", process.env.PORT || 3000);
